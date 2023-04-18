a8a4ae22ef70c9fc5fdd75d09cd5d280ca33f095
vijaypotnuru <vijaypotnuru123@gmail.com>
2023-04-18 15:26:54 +0000

    some changes made

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -8,24 +8,23 @@ const VaccinationByAge = props => {
   return (
     <div className="VaccinationByAge">
       <h1>Vaccination By Age</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={vaccinationByAge}
-            startAngle={0}
-            endAngle={360}
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="18-44" fill="#2d87bb" />
-            <Cell name="44-60" fill="#a3df9f" />
-            <Cell name="Above 60" fill="#64c2a6" />
-          </Pie>
-          <Legend iconType="circle" verticalAlign="middle" align="center" />
-        </PieChart>
-      </ResponsiveContainer>
+
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="70%"
+          cy="40%"
+          data={vaccinationByAge}
+          startAngle={0}
+          endAngle={360}
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2d87bb" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#64c2a6" />
+        </Pie>
+        <Legend iconType="circle" verticalAlign="middle" align="center" />
+      </PieChart>
     </div>
   )
 }
--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -8,25 +8,24 @@ const VaccinationByGender = props => {
   return (
     <div className="VaccinationByGender">
       <h1>Vaccination By Gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={vaccinationByGender}
-            startAngle={0}
-            endAngle={180}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Male" fill=" #f54394" />
-            <Cell name="Female" fill="#5a8dee" />
-            <Cell name="Other" fill="#2cc6c6" />
-          </Pie>
-          <Legend iconType="circle" verticalAlign="middle" align="center" />
-        </PieChart>
-      </ResponsiveContainer>
+
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="70%"
+          cy="40%"
+          data={vaccinationByGender}
+          startAngle={0}
+          endAngle={180}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill=" #f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Other" fill="#2cc6c6" />
+        </Pie>
+        <Legend iconType="circle" verticalAlign="middle" align="center" />
+      </PieChart>
     </div>
   )
 }
